8df3bb8c215e2100fa0edbc1f78644b13d52a063
roboticsza <robotics.za@gmail.com>
2019-09-07 11:35:18 +0700

    use actual component

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -11,14 +11,13 @@
 <script>
 // import PanelGroup from './components/PanelGroup'
 
-import Control from "./components/Control";
-import OEE from "./components/OEE";
-import ParetoChart from "./components/ParetoChart";
-import { SnotifyPosition } from "vue-snotify";
-import Vue from "vue";
+import Control from './components/Control'
+import OEE from './components/OEE'
+import ParetoChart from './components/ParetoChart'
+import { SnotifyPosition } from 'vue-snotify'
 
 export default {
-  name: "DashboardAdmin",
+  name: 'DashboardAdmin',
   components: {
     Control,
     OEE,
@@ -37,24 +36,24 @@ export default {
       },
       toastError: [13],
       timerToast: [13]
-    };
+    }
   },
   mqtt: {
-    "data/maintenance/pareto_chart"(data) {
-      this.objParetoChart = JSON.parse(data);
+    'data/maintenance/pareto_chart'(data) {
+      this.objParetoChart = JSON.parse(data)
     },
-    "data/maintenance/control_chart"(data) {
-      this.objControlChart = JSON.parse(data);
+    'data/maintenance/control_chart'(data) {
+      this.objControlChart = JSON.parse(data)
     },
-    "data/maintenance/alarm"(data) {
-      var obj = JSON.parse(data);
-      this.showAlarm(obj);
+    'data/maintenance/alarm'(data) {
+      var obj = JSON.parse(data)
+      this.showAlarm(obj)
     }
   },
 
   methods: {
     displayNotification(html, config = {}) {
-      return this.$snotify.html(html, config);
+      return this.$snotify.html(html, config)
     },
     getFormNotify(title, body) {
       const html = `
@@ -63,39 +62,40 @@ export default {
       </div>
       <div class="snotifyToast__body">
         ${body}
-      </div>`;
-      return html;
+      </div>`
+      return html
     },
     showAlarm(object) {
-      if (object.description_error === "clear") {
+      if (object.description_error === 'clear') {
         if (this.toastError[object.id_error] != null) {
-          this.removeNotification(this.toastError[object.id_error].id);
-          clearInterval(this.timerToast[object.id_error]);
+          this.removeNotification(this.toastError[object.id_error].id)
+          clearInterval(this.timerToast[object.id_error])
         }
       } else {
         const html = this.getFormNotify(
-          object.description_error.toUpperCase() + " ERROR!!",
+          object.description_error.toUpperCase() + ' ERROR!!',
           `M/C: ${object.mc_id}<br>
           Error station: ${object.id_error + 1}`
-        );
+        )
 
         this.toastError[object.id_error] = this.displayNotification(html, {
           timeout: 0,
-          type: "warning",
+          type: 'warning',
+          closeOnClick: false,
           position: SnotifyPosition.rightTop
-        });
+        })
 
         this.timerToast[object.id_error] = setInterval(() => {
-          this.toastError[object.id_error].config.type = "error";
-          clearInterval(this.timerToast[object.id_error]);
-        }, 15000);
+          this.toastError[object.id_error].config.type = 'error'
+          clearInterval(this.timerToast[object.id_error])
+        }, 15000)
       }
     },
     removeNotification(id) {
-      this.$snotify.remove(id);
+      this.$snotify.remove(id)
     }
   }
-};
+}
 </script>
 
 <style lang="scss">
--- src/views/icons/index.vue
+++ src/views/icons/index.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="icons-container">
     <aside>
-      <a
-        href="https://panjiachen.github.io/vue-element-admin-site/guide/advanced/icon.html"
-        target="_blank"
-      >Add and use</a>
+      <a href="https://panjiachen.github.io/vue-element-admin-site/guide/advanced/icon.html" target="_blank">Add and use
+      </a>
     </aside>
     <el-tabs type="border-card">
       <el-tab-pane label="Icons">
-        <div
-          v-for="item of svgIcons"
-          :key="item"
-          @click="handleClipboard(generateIconCode(item),$event)"
-        >
+        <div v-for="item of svgIcons" :key="item" @click="handleClipboard(generateIconCode(item),$event)">
           <el-tooltip placement="top">
-            <div slot="content">{{ generateIconCode(item) }}</div>
+            <div slot="content">
+              {{ generateIconCode(item) }}
+            </div>
             <div class="icon-item">
               <svg-icon :icon-class="item" class-name="disabled" />
               <span>{{ item }}</span>
@@ -23,13 +19,11 @@
         </div>
       </el-tab-pane>
       <el-tab-pane label="Element-UI Icons">
-        <div
-          v-for="item of elementIcons"
-          :key="item"
-          @click="handleClipboard(generateElementIconCode(item),$event)"
-        >
+        <div v-for="item of elementIcons" :key="item" @click="handleClipboard(generateElementIconCode(item),$event)">
           <el-tooltip placement="top">
-            <div slot="content">{{ generateElementIconCode(item) }}</div>
+            <div slot="content">
+              {{ generateElementIconCode(item) }}
+            </div>
             <div class="icon-item">
               <i :class="'el-icon-' + item" />
               <span>{{ item }}</span>
@@ -42,31 +36,30 @@
 </template>
 
 <script>
-import clipboard from "@/utils/clipboard";
-import svgIcons from "./svg-icons";
-import elementIcons from "./element-icons";
-import { SnotifyPosition, SnotifyToast, SnotifyToastConfig } from "vue-snotify";
+import clipboard from '@/utils/clipboard'
+import svgIcons from './svg-icons'
+import elementIcons from './element-icons'
 
 export default {
-  name: "Icons",
+  name: 'Icons',
   data() {
     return {
       svgIcons,
       elementIcons
-    };
+    }
   },
   methods: {
     generateIconCode(symbol) {
-      return `<svg-icon icon-class="${symbol}" />`;
+      return `<svg-icon icon-class="${symbol}" />`
     },
     generateElementIconCode(symbol) {
-      return `<i class="el-icon-${symbol}" />`;
+      return `<i class="el-icon-${symbol}" />`
     },
     handleClipboard(text, event) {
-      clipboard(text, event);
+      clipboard(text, event)
     }
   }
-};
+}
 </script>
 
 <style lang="scss" scoped>
